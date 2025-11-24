5c9887060464cce037d63128b3eb5545005ab19d
wangjialiang <17659909039@163.com>
2025-11-24 11:59:40 +0800

    1

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const goPoster = () => router.push('/poster')
+</script>
+
+<template>
+  <div style="padding:24px">
+    <h2>功能</h2>
+    <button @click="goPoster">海报生成器</button>
+  </div>
+</template>
